4c005b16a3a6b03e4a70aad4060c421b8cd1113a
OksanaNova <o.nova0804@gmail.com>
2025-06-13 09:50:16 -0600

    body-parser

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/form.html')
 })
 
-app.get('/vegan', (req, res) => {
-    res.send('VEGAN RECIPIES')
+app.post('/form-submitted', (req, res) => {
+    const answer = req.body.firstname + ' ' + req.body.lastname + ' ' + req.body.email + ' ' + req.body.course;
+    res.send('Your answers are: ' + ' ' + answer);
 })
+
+// app.get('/vegan', (req, res) => {
+//     res.send('VEGAN RECIPIES')
+// })
 
 app.listen(3000, () => {
     console.log(`it's working`);
